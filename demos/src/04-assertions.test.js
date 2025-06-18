9313b8c24f19d8b86224f207199f6541c3227d68
Critopher Diaz <cristopherdiaz.c@hotmail.com>
2025-06-18 16:26:13 -0400

    Clase 8: Assertions / matchers

---
+++ demos/src/04-assertions.test.js
@@ -0,0 +1,34 @@
+// Matchers
+
+test("test obj", () => {
+  const obj = { name: "Cris", age: 30 };
+  expect(obj).toEqual({ name: "Cris", age: 30 });
+});
+
+test('null', () => {
+  const a = null;
+  expect(a).toBeNull();
+  expect(a).toBeDefined();
+  expect(a).not.toBeNull();
+  // expect(a).not.toBeUndefined();
+});
+
+test('booleans', () => {
+  expect(true).toEqual(true);
+  expect(false).toEqual(false);
+
+  expect(0).toBeFalsy(false);
+  expect('').toBeFalsy(false);
+  expect(false).toBeFalsy(false);
+});
+
+test('string', () => {
+  expect('Cristopher').toMatch(/istop/);
+});
+
+test('list / array', () => {
+  const list = [1, 2, 3, 4];
+  expect(list).toContain(2);
+  expect(list).toHaveLength(4);
+  expect(list).toEqual(expect.arrayContaining([1, 2]));
+});
