2f938194c43d7a92f2cb8d2b884657bf3707d0bd
Critopher Diaz <cristopherdiaz.c@hotmail.com>
2025-06-22 19:37:42 -0400

    Clase 9 Setup and Teardown

--- demos/src/04-assertions.test.js
+++ demos/src/04-assertions.test.js
@@ -5,30 +5,30 @@ test("test obj", () => {
   expect(obj).toEqual({ name: "Cris", age: 30 });
 });
 
-test('null', () => {
-  const a = null;
-  expect(a).toBeNull();
-  expect(a).toBeDefined();
-  expect(a).not.toBeNull();
-  // expect(a).not.toBeUndefined();
-});
+// test('null', () => {
+//   const a = null;
+//   expect(a).toBeNull();
+//   expect(a).toBeDefined();
+//   expect(a).not.toBeNull();
+//   // expect(a).not.toBeUndefined();
+// });
 
-test('booleans', () => {
-  expect(true).toEqual(true);
-  expect(false).toEqual(false);
+// test('booleans', () => {
+//   expect(true).toEqual(true);
+//   expect(false).toEqual(false);
 
-  expect(0).toBeFalsy(false);
-  expect('').toBeFalsy(false);
-  expect(false).toBeFalsy(false);
-});
+//   expect(0).toBeFalsy(false);
+//   expect('').toBeFalsy(false);
+//   expect(false).toBeFalsy(false);
+// });
 
-test('string', () => {
-  expect('Cristopher').toMatch(/istop/);
-});
+// test('string', () => {
+//   expect('Cristopher').toMatch(/istop/);
+// });
 
-test('list / array', () => {
-  const list = [1, 2, 3, 4];
-  expect(list).toContain(2);
-  expect(list).toHaveLength(4);
-  expect(list).toEqual(expect.arrayContaining([1, 2]));
-});
+// test('list / array', () => {
+//   const list = [1, 2, 3, 4];
+//   expect(list).toContain(2);
+//   expect(list).toHaveLength(4);
+//   expect(list).toEqual(expect.arrayContaining([1, 2]));
+// });
