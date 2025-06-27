3aff3ca91765f01cf1e3c0e8b41b1488844a99a7
Critopher Diaz <cristopherdiaz.c@hotmail.com>
2025-06-27 17:33:46 -0400

    Clase 18 Mocking

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,14 +1,26 @@
 const BooksService = require('./books.service');
 
+const fakeBooks = [
+  { _id: '1', name: 'Book One', author: 'Author A' },
+  { _id: '2', name: 'Book Two', author: 'Author B' },
+];
+const MongoLibStub = {
+  getAll: () => fakeBooks,
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
 describe('Test for booksService', () => {
   let service;
   beforeEach(() => {
     service = new BooksService();
+    jest.clearAllMocks();
   });
 
   describe('Test for getBooks', () => {
     test('should return an array of books', async () => {
       const books = await service.getBooks();
+      // console.log('books', books);
       expect(Array.isArray(books)).toBe(true);
     });
   });
